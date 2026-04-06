4d0c7b151f1c52c3bb9423d9b7fbd88dba78c350
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-06 21:32:21 +0530

    Added design and testbench code for 2-to-4 decoder

--- combinational/decoder.v
+++ combinational/decoder.v
@@ -19,3 +19,24 @@ module decoder_2to4 (
     end
 endmodule
 
+// TB
+module tb_decoder;
+    reg [1:0] tb_in;
+    reg tb_enable;
+    wire [3:0] tb_out;
+    decoder_2to4 uut (
+        .in(tb_in),
+        .enable(tb_enable),
+        .out(tb_out)
+    );
+    initial begin
+        $monitor("Time=%0t | Enable=%b | Input=%b | Output=%b", $time, tb_enable, tb_in, tb_out);
+        tb_enable = 0; tb_in = 2'b00; #10;
+        tb_enable = 1;
+        tb_in = 2'b00; #10;
+        tb_in = 2'b01; #10;
+        tb_in = 2'b10; #10;
+        tb_in = 2'b11; #10;
+        $finish;
+    end
+endmodule
