374f1eccfe603069b92d90b1a1a66ae9c9e36610
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-07 10:36:28 +0530

    Updated Code

--- and_gate.v
+++ and_gate.v
@@ -1,28 +1,20 @@
-// And Gate Design Code
-module and_gate (
-    input a,
-    input b,
-    output y
-);
-assign y = a & b;
+// a->I1, b->I2, y->O1, &->AND Operator
+// Design code for AND Gate
+module andgate(a,b,y);
+  input a,b;
+  output y;
+  assign y=a&b;
 endmodule
 
-// TB Code
-module and_gate_tb;
-reg a, b;
+// TB 
+module andtb;
 wire y;
-and_gate uut (
-    .a(a),
-    .b(b),
-    .y(y)
-);
-initial begin
-    $display("a b | y");
-    $monitor("%b %b | %b", a, b, y);
-    a = 0; b = 0;
-    #10 a = 0; b = 1;
-    #10 a = 1; b = 0;
-    #10 a = 1; b = 1;
-    #10 $finish;
-end
+reg a,b;
+  andgate dut(a,b,y);
+  initial begin
+   a=0;b=0;
+    #1 a=0;b=1;
+    #1 a=1;b=0;
+    #1 a=1;b=1;
+  end
 endmodule
