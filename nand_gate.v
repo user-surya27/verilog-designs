87037266bd29c0f87c66be27eaf133a1af64c8a4
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-07 10:49:21 +0530

    Updated Code

--- nand_gate.v
+++ nand_gate.v
@@ -1,29 +1,19 @@
-// Design code for nand gate
-module nand_gate(
-    input a,
-    input b,
-    output y
-);
-    assign y = ~(a & b);
-endmodule
+// Design code for NAND Gate
+module nandgate(a,b,y);
+  input a,b;
+  output y;
+  assign y=~(a&b);
+  endmodule
 
 // TB
-module nand_gate_tb;
-    reg a;
-    reg b;
-    wire y;
-    nand_gate uut (
-        .a(a),
-        .b(b),
-        .y(y)
-    );
-    initial begin
-        $monitor("Time=%0t | a=%b b=%b | y=%b", $time, a, b, y);
-        $dumpfile("dump.vcd"); $dumpvars;
-        a = 0; b = 0; #10;
-        a = 0; b = 1; #10;
-        a = 1; b = 0; #10;
-        a = 1; b = 1; #10;
-        $finish;
-    end
+module nandtb;
+  reg a,b;
+  wire y;
+  nandgate dut(a,b,y);
+  initial begin 
+    a=0;b=0;
+    #1 a=0;b=1;
+    #1 a=1;b=0;
+    #1 a=1;b=1;
+  end
 endmodule
