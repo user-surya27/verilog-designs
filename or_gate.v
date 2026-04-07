672a787a004e4ce16d87025cee2afc8cbe1a498d
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-07 10:30:13 +0530

    Updated Code

--- or_gate.v
+++ or_gate.v
@@ -1,28 +1,19 @@
-// Or gate Design code
-module or_gate (
-    input a,
-    input b,
-    output y
-);
-assign y = a | b;
+// Design  code for OR Gate
+module orgate(a,b,y);
+  input a,b;
+  output y;
+  assign y=a|b;
 endmodule
 
-// TB code
-module or_gate_tb;
-reg a, b;
+// TB 
+module ortb;
 wire y;
-or_gate uut (
-    .a(a),
-    .b(b),
-    .y(y)
-);
-initial begin
-    $display("a b | y");
-    $monitor("%b %b | %b", a, b, y);
-    a = 0; b = 0;
-    #10 a = 0; b = 1;
-    #10 a = 1; b = 0;
-    #10 a = 1; b = 1;
-    #10 $finish;
-end
+reg a,b;
+  orgate dut(a,b,y);
+  initial begin
+    a=0;b=0;
+    #1 a=0;b=1;
+    #1 a=1;b=0;
+    #1 a=1;b=1;
+  end
 endmodule
