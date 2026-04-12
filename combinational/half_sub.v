66ff26cffceb4f8cdded22eb58350840e6d9a023
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-12 19:57:42 +0530

    Implement half subtractor in Verilog
    Added Verilog code for a half subtractor module.

--- combinational/half_sub.v
+++ combinational/half_sub.v
@@ -1,1 +1,11 @@
+// Design code for Half Subtractor 
+module half_subtractor (
+    input a,      
+    input b,     
+    output diff,  
+    output borrow  
+);
+    assign diff = a ^ b;
+    assign borrow = (~a) & b;
+endmodule
 
