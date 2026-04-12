d5437d1701a1b290ef9a6566bcf731ca5b840fc1
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-12 19:59:53 +0530

    Updated Code
    Added a testbench for the half subtractor module to verify functionality.

--- combinational/half_sub.v
+++ combinational/half_sub.v
@@ -9,3 +9,24 @@ module half_subtractor (
     assign borrow = (~a) & b;
 endmodule
 
+// TB
+module half_subtractor_tb;
+    reg a;
+    reg b;
+    wire diff;
+    wire borrow;
+    half_subtractor uut (
+        .a(a),
+        .b(b),
+        .diff(diff),
+        .borrow(borrow)
+    );
+    initial begin
+        $monitor("Time=%0t | a=%b b=%b | Diff=%b Borrow=%b", $time, a, b, diff, borrow);
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+        $finish; 
+    end
+endmodule
