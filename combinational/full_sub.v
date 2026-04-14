a0c7dd62d35afcd520e1da0baee2ce9da4e37e1c
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-14 20:26:31 +0530

    Added code for full subtractor

--- combinational/full_sub.v
+++ combinational/full_sub.v
@@ -10,3 +10,28 @@ module full_subtractor(
     assign Bout = (~A & B) | (~(A ^ B) & Bin); 
 endmodule
 
+// TB
+module tb_full_subtractor;
+    reg A, B, Bin;
+    wire Diff, Bout;
+    full_subtractor uut (
+        .A(A), 
+        .B(B), 
+        .Bin(Bin), 
+        .Diff(Diff), 
+        .Bout(Bout)
+    );
+    initial begin
+        $display("A B Bin | Diff Bout");
+        $monitor("%b %b  %b  |  %b    %b", A, B, Bin, Diff, Bout);
+        A = 0; B = 0; Bin = 0; #10;
+        A = 0; B = 0; Bin = 1; #10;
+        A = 0; B = 1; Bin = 0; #10;
+        A = 0; B = 1; Bin = 1; #10;
+        A = 1; B = 0; Bin = 0; #10;
+        A = 1; B = 0; Bin = 1; #10;
+        A = 1; B = 1; Bin = 0; #10;
+        A = 1; B = 1; Bin = 1; #10;
+        $finish;
+    end
+endmodule
