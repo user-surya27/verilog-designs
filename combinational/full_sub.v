e347b0f6b65295d267f4299143e38661ad204ab9
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-14 20:22:34 +0530

    Added design code for full sub

--- combinational/full_sub.v
+++ combinational/full_sub.v
@@ -1,1 +1,12 @@
-// This file is for full subtractor
+// Design code for Full Subtractor 
+module full_subtractor(
+    input A,
+    input B,
+    input Bin,
+    output Diff,
+    output Bout
+);
+    assign Diff = A ^ B ^ Bin;
+    assign Bout = (~A & B) | (~(A ^ B) & Bin); 
+endmodule
+
