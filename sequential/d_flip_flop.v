56e408bb0a6c240ce070e179d04521ff0d7379b3
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-16 21:05:27 +0530

    Added design code for D flip flop

--- sequential/d_flip_flop.v
+++ sequential/d_flip_flop.v
@@ -1,1 +1,15 @@
-// This file is for D Flip Flop
+// Design code for D Flip Flop
+module d_ff (
+    input clk,     
+    input rst_n,  
+    input d,      
+    output reg q  
+);
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            q <= 1'b0; 
+        end else begin
+            q <= d;   
+        end
+    end
+endmodule
