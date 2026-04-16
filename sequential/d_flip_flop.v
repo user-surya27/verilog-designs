7124b8c7a0cbab11e0cd216e4930b381da20bfae
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-16 21:08:38 +0530

    Added Design and Testbench code for D Flip Flop

--- sequential/d_flip_flop.v
+++ sequential/d_flip_flop.v
@@ -13,3 +13,30 @@ module d_ff (
         end
     end
 endmodule
+
+// TB
+module d_ff_tb;
+    reg clk;
+    reg rst_n;
+    reg d;
+    wire q;
+    d_ff uut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .d(d),
+        .q(q)
+    );
+    always #5 clk = ~clk;
+    initial begin
+        clk = 0;
+        rst_n = 0; 
+        d = 0;
+        #20 rst_n = 1;
+        #10 d = 1; 
+        #10 d = 0; 
+        #20 d = 1;
+        #15 rst_n = 0; 
+        #10 rst_n = 1;
+        #50 $finish; 
+    end
+endmodule
