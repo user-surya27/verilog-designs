64f8d222c670a91c5ab8507d455fd4b7c0856242
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-17 20:15:55 +0530

    Added design and testbench code for SR Flip Flop

--- sequential/sr_flip_flop.v
+++ sequential/sr_flip_flop.v
@@ -23,3 +23,28 @@ module sr_ff (
     end
 endmodule
 
+// TB
+module sr_ff_tb;
+    reg clk;
+    reg rst;
+    reg s, r;
+    wire q, q_bar;
+    sr_ff dut (
+        .clk(clk),
+        .rst(rst),
+        .s(s),
+        .r(r),
+        .q(q),
+        .q_bar(q_bar)
+    );
+    always #5 clk = ~clk;
+    initial begin
+        clk = 0; rst = 1; s = 0; r = 0;
+        #10 rst = 0;
+        #10 s = 1; r = 0;
+        #10 s = 0; r = 0;
+        #10 s = 0; r = 1;
+        #10 s = 1; r = 1;
+        #20 $finish;
+    end
+endmodule
