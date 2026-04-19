9c8e7ba340a906756bbda2d4242e76a42f135ba3
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-19 20:00:39 +0530

    Added Design code for T Flip Flop

--- sequential/t_flip_flop.v
+++ sequential/t_flip_flop.v
@@ -1,1 +1,18 @@
-// this file is for T Flip Flop
+// Design Code for T Flip Flop
+module tff (
+    input clk,  
+    input rstn, 
+    input t,    
+    output reg q 
+);
+    always @(posedge clk or negedge rstn) begin
+        if (!rstn) begin
+            q <= 1'b0;    
+        end else if (t) begin
+            q <= ~q;      
+        end else begin
+            q <= q;     
+        end
+    end
+endmodule
+
