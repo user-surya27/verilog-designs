87145fd0b82eb7dba8be37f7d304aca8a29155f7
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-19 20:02:59 +0530

    Added Design and Testbench code for T Flip Flop
    Added testbench for the T Flip-Flop module to verify functionality.

--- sequential/t_flip_flop.v
+++ sequential/t_flip_flop.v
@@ -16,3 +16,28 @@ module tff (
     end
 endmodule
 
+// TB
+module tff_tb;
+    reg clk;
+    reg rstn;
+    reg t;
+    wire q;
+    tff uut (
+        .clk(clk),
+        .rstn(rstn),
+        .t(t),
+        .q(q)
+    );
+    always #5 clk = ~clk;
+    initial begin
+        clk = 0;
+        rstn = 0;
+        t = 0;
+        #15 rstn = 1;
+        #10 t = 1;  
+        #20 t = 0; 
+        #20 t = 1; 
+        #20 t = 1;  
+        #50 $finish; 
+    end
+endmodule
