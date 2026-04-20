d5c755460d67986386e66b71cdcab824867bb677
Surya Velliyangiri <tyzusurya@gmail.com>
2026-04-20 20:48:14 +0530

    Update comments and format for 4 Bit Binary Counter

--- sequential/4_bit_binary_counter.v
+++ sequential/4_bit_binary_counter.v
@@ -1,1 +1,15 @@
-// This file is for 4 Bit Binary Counter
+// Design code for 4 Bit Binary Counter
+module counter_4bit (
+    input clk,     
+    input reset,   
+    output reg [3:0] out 
+);
+always @(posedge clk) begin
+    if (reset) begin
+        out <= 4'b0000; 
+    end else begin
+        out <= out + 1;
+    end
+end
+endmodule
+
